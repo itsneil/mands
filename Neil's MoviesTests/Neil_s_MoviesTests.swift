2e3a1f836c4025acd66881aeb5fbeda8a1ebf07d
Neil Skinner <me@npskinner.com>
2018-11-05 19:32:53 +0000

    Initial Commit

---
+++ Neil's MoviesTests/Neil_s_MoviesTests.swift
@@ -0,0 +1,34 @@
+//
+//  Neil_s_MoviesTests.swift
+//  Neil's MoviesTests
+//
+//  Created by Neil Skinner on 05/11/2018.
+//  Copyright © 2018 Neil Skinner. All rights reserved.
+//
+
+import XCTest
+@testable import Neil_s_Movies
+
+class Neil_s_MoviesTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
